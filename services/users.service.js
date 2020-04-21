9fbd3587068624d892aa7be429d933e8301f57c7
Vahe_Gevorgyan1 <Vahe_Gevorgyan1@epam.com>
2020-04-21 20:53:30 +0400

    homework3

--- services/users.service.js
+++ services/users.service.js
@@ -1,75 +1,25 @@
-import crypto from 'crypto';
-import { v4 as uuidv4 } from 'uuid';
+import Users from '../models/users.model';
 
-class UserService {
-    constructor() {
-        this.users = [];
-    }
-
-    getUsers() {
-        return Promise.resolve(this.users.filter(val => !val.isDeleted));
-    }
-
-    getAutoSuggestUsers(loginSubstring, limit) {
-        const sugestedUsers = this.users.filter(val =>
-            val.login.match(new RegExp(`${ loginSubstring }`) && !val.isDeleted)
-        );
-
-        if (sugestedUsers && sugestedUsers.length > 0) {
-            return Promise.resolve(sugestedUsers.slice(0, limit));
-        }
-        return Promise.reject();
-    }
-
-    getUserById(id) {
-        let user = this.users.find(val => !val.isDeleted && val.id === id);
-        user = user ? { ...user } : null;
-        return user
-            ? delete user.password && delete user.isDeleted && Promise.resolve(user)
-            : Promise.reject('not found');
-    }
-
-    create(data) {
-        const id = uuidv4();
-        const password = crypto
-            .createHmac('sha256', id)
-            .update(data.password)
-            .digest('hex');
-        const user = {
-            ...data,
-            id,
-            password,
-            isDeleted: false
-        };
-        this.users.push({ ...user });
-        delete user.password;
-        return Promise.resolve(user);
-    }
-
-    update(data, id) {
-        let updated;
-        this.users = this.users.map(val => {
-            if (id === val.id) {
-                updated = {
-                    ...val,
-                    ...data,
-                    password: crypto
-                        .createHmac('sha256', val.id)
-                        .update(data.password)
-                        .digest('hex')
-                };
-                return updated;
-            }
-            return val;
-        });
-        return updated ? Promise.resolve() : Promise.reject('not found');
-    }
-
+class UsersService {
     delete(id) {
-        let status = false;
-        this.users = this.users.map(val => val.id === id ? status = true && { ...val, isDeleted: true } : val);
-        return status ? Promise.resolve(true) : Promise.reject();
+        return Users.destroy({ where: { id } });
+    }
+
+    getAllUsers() {
+        return Users.findAll();
+    }
+
+    save(body) {
+        return Users.create(body);
+    }
+
+    update(id, body) {
+        return Users.update(body, { where: { id }, returning: true });
+    }
+
+    findById(id) {
+        return Users.findByPk(id);
     }
 }
 
-module.exports = new UserService();
+export default new UsersService();
