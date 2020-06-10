e4734a948d35264ed51ded8c3e3512c42b546869
Vahe_Gevorgyan1 <Vahe_Gevorgyan1@epam.com>
2020-06-10 11:49:59 +0400

    fix(JWT) Custom middleware

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,7 @@
-import passport from '../services/auth.service';
+import { JWTMiddleware } from '../services/auth.service';
 
 const routes = [
     { route: '/users', path: './users' },
     { route: '/groups', path: './user-groups' }
 ];
-module.exports = (app) => routes.map(value => app.use(value.route, passport.authenticate('jwt', { session: false }), require(value.path)));
+module.exports = (app) => routes.map(value => app.use(value.route, JWTMiddleware, require(value.path)));
--- services/auth.service.js
+++ services/auth.service.js
@@ -1,62 +1,50 @@
 import User from '../models/users.model';
 import passport from 'passport';
+import jwt from 'jsonwebtoken';
 
 require('dotenv').config();
 const LocalStrategy = require('passport-local').Strategy;
-const JwtStrategy = require('passport-jwt').Strategy;
-const ExtractJwt = require('passport-jwt').ExtractJwt;
 
-
-// passport.use('signup', new LocalStrategy({
-//     usernameField : 'email',
-//     passwordField : 'password'
-// }, async (email, password, done) => {
-//     try {
-//         //Save the information provided by the user to the the database
-//         const user = await UserModel.create({ email, password });
-//         //Send the user information to the next middleware
-//         return done(null, user);
-//     } catch (error) {
-//         done(error);
-//     }
-// }));
-
-//Create a passport middleware to handle User login
-passport.use('login', new LocalStrategy({
+// Create a passport middleware to handle User login
+passport.use('local', new LocalStrategy({
     usernameField: 'login',
     passwordField: 'password'
 }, async (login, password, done) => {
     try {
-        //Find the user associated with the login provided by the user
+        // Find the user associated with the login provided by the user
         const user = await User.findOne({ where: { login } });
         if (!user) {
-            //If the user isn't found in the database, return a message
+            // If the user isn't found in the database, return a message
             return done(null, false, { message: 'User not found' });
         }
-        //Validate password and make sure it matches with the corresponding hash stored in the database
-        //If the passwords match, it returns a value of true.
+        // Validate password and make sure it matches with the corresponding hash stored in the database
+        // If the passwords match, it returns a value of true.
         const validate = await user.validPassword(password);
         if (!validate) {
             return done(null, false, { message: 'Wrong Password' });
         }
-        //Send the user information to the next middleware
+        // Send the user information to the next middleware
         return done(null, user, { message: 'Logged in Successfully' });
     } catch (error) {
         return done(error);
     }
 }));
 
-//This verifies that the token sent by the user is valid
-passport.use(new JwtStrategy({
-    jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
-    secretOrKey: process.env.SECRET_KEY
-}, async (token, done) => {
-    try {
-        //Pass the user details to the next middleware
-        return done(null, token.user);
-    } catch (error) {
-        done(error);
+const JWTMiddleware = (req, res, next) => {
+    if (!req.get('Authorization')) {
+        res.status(401).json({ error: 'Unauthorized' });
+    } else {
+        try {
+            const decode = jwt.verify(req.get('Authorization'), process.env.SECRET_KEY, {});
+            if (decode.user) {
+                req.user = decode.user;
+                next();
+            }
+        } catch (e) {
+            res.status(403).json({ error: 'Forbidden' });
+        }
     }
-}));
+};
 
-module.exports = passport;
+
+module.exports = { passport, JWTMiddleware };
