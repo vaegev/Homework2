f126cc698cd3f462512b663396b7169515b03d25
Vahe_Gevorgyan1 <Vahe_Gevorgyan1@epam.com>
2020-06-15 15:42:33 +0400

    fix(dead code) dead code removal in test files

--- app.test.js
+++ app.test.js
@@ -1,16 +1,7 @@
 const request = require('supertest');
 const app = require('./app');
-// const db = require('./config');
 
 describe('Test the root path', () => {
-    // beforeAll(async () => {
-    //     await db.authenticate();
-    //     await db.sync();
-    // });
-    //
-    // afterAll(() => {
-    //     db.close();
-    // });
     test('It should response the GET method', async (done) => {
         try {
             const token = await request(app).post('/auth/login').send({
