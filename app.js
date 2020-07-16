7ed93931895c810649db537f9a3a26c99fba1d16
Vahe_Gevorgyan1 <Vahe_Gevorgyan1@epam.com>
2020-07-16 11:27:14 +0400

    feat(app) Helmet Compression added

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 import express from 'express';
 import path from 'path';
+import compression from 'compression';
+import helmet from 'helmet';
 import cookieParser from 'cookie-parser';
 import logger from 'morgan';
 import routes from './routes/index';
@@ -11,6 +13,8 @@ import swaggerDocument from './swagger';
 const app = express();
 
 app.use(cors());
+app.use(compression());
+app.use(helmet());
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
